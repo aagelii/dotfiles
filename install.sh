786817ba9eb6f77d9476012579a62c7b69acb181
Axel Agelii <aagelii17@gmail.com>
2025-04-08 20:46:42 -0400

    change install to clone my repo

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ DOTFILES=$HOME/dotfiles
 
 if [[ "$(uname)" == "Darwin" ]]; then
   echo "Configuring MacOS"
-  git clone https://github.com/rchhong/dotfiles.git $DOTFILES
+  git clone https://github.com/aagelii/dotfiles.git $DOTFILES
   $DOTFILES/osx/install_osx.sh
   cd $DOTFILES
 elif [[ "$(uname)" == "Linux" ]]; then
   if [[ -f "/etc/arch-release" ]]; then
     echo "Configuring Arch Linux (Manjaro)"
-    git clone https://github.com/rchhong/dotfiles.git $DOTFILES
+    git clone https://github.com/aagelii/dotfiles.git $DOTFILES
     cd $DOTFILES
     $DOTFILES/arch/install_arch.sh
   else
@@ -20,7 +20,7 @@ elif [[ "$(uname)" == "Linux" ]]; then
     sudo apt-get update
     sudo apt install -y git
 
-    git clone https://github.com/rchhong/dotfiles.git $DOTFILES
+    git clone https://github.com/aagelii/dotfiles.git $DOTFILES
     cd $DOTFILES
     $DOTFILES/ubuntu/install_ubuntu.sh
   fi
